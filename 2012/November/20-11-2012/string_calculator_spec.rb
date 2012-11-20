2baebdb06081dfef622e726e84b9124d2b396c63
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-20 10:05:22 +0100

    Multiple number Test Calc#add green

--- 2012/November/20-11-2012/string_calculator_spec.rb
+++ 2012/November/20-11-2012/string_calculator_spec.rb
@@ -7,8 +7,16 @@ describe 'StringCalculator' do
       calc.add("").should == 0
 		end
 
+    it 'returns 1' do
+      calc.add("1").should == 1
+    end
+
     it 'returns 3' do
       calc.add("1,2").should == 3
+    end
+
+    it 'returns 44' do
+      calc.add("20, 10, 5, 5, 4").should == 44
     end
 
 	end
