72a0039388eb4e0d591674c397ae75514953b337
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-21 08:52:01 +0100

    Step 2 test all ones red

--- 2012/November/21-11-2012/bowling_game_spec.rb
+++ 2012/November/21-11-2012/bowling_game_spec.rb
@@ -3,10 +3,20 @@ require_relative 'bowling_game'
 describe 'BownlingGame' do
   describe 'Gutter game' do
     game = Game.new
-    1.upto(10).each do |roll|
+    1.upto(20).each do |roll|
       game.roll(0)
     end
 
     game.score.should == 0
   end
+
+  describe 'Roll one pin per throw' do
+    game = Game.new
+    1.upto(20).each do |roll|
+      game.roll(1)
+    end
+
+    game.score.should == 20
+  end
+
 end
