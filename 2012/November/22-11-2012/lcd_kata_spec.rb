934d7ec5295e56333813fb8c036a9c1c7a957e6f
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-22 09:02:41 +0100

    Step 1, Convert 1 red

--- 2012/November/22-11-2012/lcd_kata_spec.rb
+++ 2012/November/22-11-2012/lcd_kata_spec.rb
@@ -1,5 +1,11 @@
 require_relative 'lcd_kata'
 
 describe 'Lcd Kata' do
-
+  it "should return the LCD representation of 1" do
+    Lcd.convert(1).should ==  "   " +
+                              "  |" +
+                              "   " +
+                              "  |" +
+                              "   "
+  end
 end
