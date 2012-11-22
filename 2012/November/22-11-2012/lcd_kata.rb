17fa952a1e67ec8fdbbf234fc83ac3d6125270a3
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-22 08:50:11 +0100

    Daily Kata for 22 November: Number to LCD kata

---
+++ 2012/November/22-11-2012/lcd_kata.rb
@@ -0,0 +1,119 @@
+# Create a system that will convert a numeric value to an 3x5 LED
+# display.
+# ...
+# ...
+# ...
+# ...
+# ...
+
+# Feature: Generating LCD representation for single numbers
+#
+#  Scenario: Number 1
+#    When I convert the number "1" to LCD
+#    Then I should get the following representation
+#    """
+#       
+#      |
+#       
+#      |
+#       
+#    """
+
+#  Scenario: Number 2
+#    When I convert the number "2" to LCD
+#    Then I should get the following representation
+#    """
+#     - 
+#      |
+#     - 
+#    |  
+#     - 
+#    """
+
+#  Scenario: Number 3
+#    When I convert the number "3" to LCD
+#    Then I should get the following representation
+#    """
+#     - 
+#      |
+#     - 
+#      |
+#     - 
+#    """
+
+#  Scenario: Number 4
+#    When I convert the number "4" to LCD
+#    Then I should get the following representation
+#    """
+#       
+#    | |
+#     - 
+#      |
+#       
+#    """
+
+#  Scenario: Number 5
+#    When I convert the number "5" to LCD
+#    Then I should get the following representation
+#    """
+#     - 
+#    |  
+#     - 
+#      |
+#     - 
+#    """
+  
+#  Scenario: Number 6
+#    When I convert the number "6" to LCD
+#    Then I should get the following representation
+#    """
+#     - 
+#    |  
+#     - 
+#    | |
+#     - 
+#    """
+
+#  Scenario: Number 7
+#    When I convert the number "7" to LCD
+#    Then I should get the following representation
+#    """
+#     - 
+#      |
+#       
+#      |
+#       
+#    """
+
+#  Scenario: Number 8
+#    When I convert the number "8" to LCD
+#    Then I should get the following representation
+#    """
+#     - 
+#    | |
+#     - 
+#    | |
+#     - 
+#    """
+
+#  Scenario: Number 9
+#    When I convert the number "9" to LCD
+#    Then I should get the following representation
+#    """
+#     - 
+#    | |
+#     - 
+#      |
+#     - 
+#    """
+
+#  Scenario: Number 0
+#    When I convert the number "0" to LCD
+#    Then I should get the following representation
+#    """
+#     - 
+#    | |
+#       
+#    | |
+#     - 
+#    """
---
+++ 2012/November/22-11-2012/lcd_kata_spec.rb
@@ -0,0 +1,5 @@
+require_relative 'lcd_kata'
+
+describe 'Lcd Kata' do
+
+end
