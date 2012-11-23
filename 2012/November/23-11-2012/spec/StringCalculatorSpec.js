cba58712c063f738b69f64d37e10bceda14ec15c
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-23 09:24:16 +0100

    Step 2, Convert 1 green, refactored tests

--- 2012/November/23-11-2012/spec/StringCalculatorSpec.js
+++ 2012/November/23-11-2012/spec/StringCalculatorSpec.js
@@ -1,14 +1,15 @@
 describe("StringCalculator", function() {
+  var stringCalculator;
+
+  beforeEach(function() {
+    stringCalculator = new StringCalculator();
+  })
 
   it("should return 0 given empty string", function() {
-    stringCalculator = new StringCalculator();
-
     expect(stringCalculator.add("")).toEqual(0);
-  })
+  });
 
   it("should return 1 given '1'", function() {
-    stringCalculator = new StringCalculator();
-
     expect(stringCalculator.add("1")).toEqual(1);
-  })
+  });
 })
