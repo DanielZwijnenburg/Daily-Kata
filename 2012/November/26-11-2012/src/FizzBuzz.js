6ca3a4dd80aea448588dde56d58eefa011432e37
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-26 10:56:33 +0100

    FizzBuzz, Step 5 refactor commin interface green

--- 2012/November/26-11-2012/src/FizzBuzz.js
+++ 2012/November/26-11-2012/src/FizzBuzz.js
@@ -2,24 +2,30 @@ function FizzBuzz(){
 
 }
 
-FizzBuzz.prototype.calculate = function(number){
-  if(number % 3 == 0){
+FizzBuzz.prototype.calculate = function(number) {
+  if(number %3 == 0){
     return 'fizz';
   }
 }
 
-
-FizzBuzz.prototype.calculateFizz = function(number){
-  return number % 3 == 0
+FizzBuzz.prototype.calculateFizz = function(number) {
+  if (number % 3 == 0) {
+    return 'fizz';
+  }
 }
 
-FizzBuzz.prototype.calculateBuzz = function(number){
+FizzBuzz.prototype.isMultipleOfThree = function(number) {
+  return number % 3 == 0;
+}
+
+
+FizzBuzz.prototype.calculateBuzz = function(number) {
   if(number % 5 == 0){
     return 'buzz';
   }
 }
 
-FizzBuzz.prototype.calculateFizzBuzz = function(number){
+FizzBuzz.prototype.calculateFizzBuzz = function(number) {
   if(number % 15 == 0){
     return 'fizzbuzz';
   }
