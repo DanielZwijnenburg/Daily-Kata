117e34e029a6e7884c0472ac3e15124cafc960cd
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-26 11:01:11 +0100

    FizzBuzz, Step 6 return fizzbuzz via commin interface given 15 green

--- 2012/November/26-11-2012/src/FizzBuzz.js
+++ 2012/November/26-11-2012/src/FizzBuzz.js
@@ -3,10 +3,13 @@ function FizzBuzz(){
 }
 
 FizzBuzz.prototype.calculate = function(number) {
-  if(number %3 == 0){
+  if(number % 3 == 0 && number % 5 == 0){
+    return 'fizzbuzz';
+  }
+  if(number % 3 == 0){
     return 'fizz';
   }
-    if(number %5 == 0){
+    if(number % 5 == 0){
     return 'buzz';
   }
 }
