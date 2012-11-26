09b864d17697398de099d61bd245257d6e15e3da
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-26 10:04:35 +0100

    FizzBuzz, Step 1 return fizz red

--- 2012/November/26-11-2012/spec/FizzBuzzSpec.js
+++ 2012/November/26-11-2012/spec/FizzBuzzSpec.js
@@ -1,2 +1,6 @@
 describe("FizzBuzz", function() {
+  it("should return fizz given 3", function() {
+    var fizzBuzz = new FizzBuzz()
+    expect(fizzBuzz.calculateFizz(3)).toEqual("fizz");
+  });
 })
