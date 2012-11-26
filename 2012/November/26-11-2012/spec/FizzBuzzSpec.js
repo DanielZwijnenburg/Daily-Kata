cfd383f0eb3758dc6d8e95ed739823ca59223bf0
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-26 10:09:56 +0100

    FizzBuzz, Step 1 refactor

--- 2012/November/26-11-2012/spec/FizzBuzzSpec.js
+++ 2012/November/26-11-2012/spec/FizzBuzzSpec.js
@@ -1,16 +1,18 @@
 describe("FizzBuzz", function() {
+  var fizzBuzz;
+
+  beforeEach(function(){
+    fizzBuzz = new FizzBuzz();
+  })
   it("should return fizz given 3", function() {
-    var fizzBuzz = new FizzBuzz()
     expect(fizzBuzz.calculateFizz(3)).toEqual("fizz");
   });
 
   it("should return buzz given 5", function() {
-    var fizzBuzz = new FizzBuzz()
     expect(fizzBuzz.calculateBuzz(5)).toEqual("buzz");
   });
 
   it("should return fizzbuzz given 15", function() {
-    var fizzBuzz = new FizzBuzz()
     expect(fizzBuzz.calculateFizzBuzz(15)).toEqual("fizzbuzz");
   });
 })
