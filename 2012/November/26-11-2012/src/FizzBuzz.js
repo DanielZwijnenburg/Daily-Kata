830db36c5db7b6fb59f1de5794f2f385101b71a3
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-26 11:02:52 +0100

    FizzBuzz, Step 7 return number via defaul tinterface if not dividabe by 3, 5 or 3 and 5 green

--- 2012/November/26-11-2012/src/FizzBuzz.js
+++ 2012/November/26-11-2012/src/FizzBuzz.js
@@ -9,9 +9,11 @@ FizzBuzz.prototype.calculate = function(number) {
   if(number % 3 == 0){
     return 'fizz';
   }
-    if(number % 5 == 0){
+  if(number % 5 == 0){
     return 'buzz';
   }
+
+  return number;
 }
 
 FizzBuzz.prototype.calculateFizz = function(number) {
