b13ab1d7379bd2716c7da4ea67d32f6ff87d109a
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-27 12:43:26 +0100

    Should return the number by default red

--- 2012/November/27-11-2012/fizzbuzz_spec.rb
+++ 2012/November/27-11-2012/fizzbuzz_spec.rb
@@ -2,6 +2,7 @@ require_relative 'fizzbuzz'
 
 describe 'FizzBuzz' do
   it "should return the number by default" do
+    FizzBuzz.new.calculate(1).should == "1"
   end
 
   it "should return 'fizz' when number is dividable by 3" do
