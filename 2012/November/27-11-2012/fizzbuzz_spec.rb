13b4bb3ae7b32bd4685437dff54412235808f68b
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-27 12:41:55 +0100

    FizzBuzz tests

--- 2012/November/27-11-2012/fizzbuzz_spec.rb
+++ 2012/November/27-11-2012/fizzbuzz_spec.rb
@@ -1,5 +1,16 @@
 require_relative 'fizzbuzz'
 
 describe 'FizzBuzz' do
-  
+  it "should return the number by default" do
+  end
+
+  it "should return 'fizz' when number is dividable by 3" do
+  end
+
+  it "should return 'buzz' when number is dividable by 5" do
+
+  end
+
+  it "should return 'fizzbuzz' when number is dividable by 15" do
+  end
 end
