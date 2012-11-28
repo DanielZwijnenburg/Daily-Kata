547d01a510e12bfb1c296e6cce4ee0eed251e1e5
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-28 08:35:36 +0100

    Step 3, it should factor 4 green

--- 2012/November/28-11-2012/prime_factors.rb
+++ 2012/November/28-11-2012/prime_factors.rb
@@ -2,7 +2,8 @@ class PrimeFactors
   def self.of(number)
     factors = []
     if number > 1
-      factors << number
+      factors << 2 if number % 2 == 0 and number /= 2
+      factors << number if number > 1
     end
     factors
   end
