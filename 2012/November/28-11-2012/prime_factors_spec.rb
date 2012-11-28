8471cb62774c630961a4fe73063f72a3dd6500e9
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-28 08:36:18 +0100

    Step 3, it should factor 5 green

--- 2012/November/28-11-2012/prime_factors_spec.rb
+++ 2012/November/28-11-2012/prime_factors_spec.rb
@@ -5,7 +5,8 @@ describe 'PrimeFactors' do
     ['1', []],
     ['2', [2]],
     ['3', [3]],
-    ['4', [2,2]]
+    ['4', [2,2]],
+    ['5', [5]]
   ].each do |expression, factors|
     it "should factor #{expression}" do
       number = eval expression
