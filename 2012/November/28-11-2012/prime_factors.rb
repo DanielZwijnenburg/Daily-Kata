7a1983260c4293bf0a9188c4a4a9eccb9cb8552b
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-28 08:39:18 +0100

    Step 6, it should factor 8 green

--- 2012/November/28-11-2012/prime_factors.rb
+++ 2012/November/28-11-2012/prime_factors.rb
@@ -2,7 +2,7 @@ class PrimeFactors
   def self.of(number)
     factors = []
     if number > 1
-      factors << 2 if number % 2 == 0 and number /= 2
+      factors << 2 while number % 2 == 0 and number /= 2
     end
     factors << number if number > 1
     factors
