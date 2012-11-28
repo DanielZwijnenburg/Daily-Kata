c6beed78d292549380924708802b820de3366487
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-28 08:31:04 +0100

    Step 3, it should factor 3 red

--- 2012/November/28-11-2012/prime_factors_spec.rb
+++ 2012/November/28-11-2012/prime_factors_spec.rb
@@ -3,7 +3,8 @@ require_relative 'prime_factors'
 describe 'PrimeFactors' do
   [
     ['1', []],
-    ['2', [2]]
+    ['2', [2]],
+    ['3', [3]]
   ].each do |expression, factors|
     it "should factor #{expression}" do
       number = eval expression
