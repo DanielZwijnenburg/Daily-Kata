5c9e868ae1030b765671b97f2df18606d77966ce
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-29 12:31:26 +0100

    Should convert 6 to VI green

--- 2012/November/29-11-2012/roman_numeral.rb
+++ 2012/November/29-11-2012/roman_numeral.rb
@@ -3,14 +3,33 @@
 # http://codingdojo.org/cgi-bin/wiki.pl?KataRomanNumerals
 #
 class RomanNumeral
+  NUMERALS = {
+      1000 => 'M' ,
+      900 => 'CM',
+      500 => 'D',
+      400 => 'CD',
+      100 => 'C',
+      90 => 'XC',
+      50 => 'L',
+      40 => 'XL',
+      10 => 'X',
+      9 => 'IX',
+      5 => 'V',
+      4 => 'IV',
+      1 => 'I'
+  }
 
   def toRoman(number)
     num = ""
     number.times do |n|
       if number <= 4
         num += "I"
-      else
+      elsif number == 5
         num += "V"
+        break
+      elsif number > 5
+        num += "V"
+        num+= "I"
         break
       end
     end
