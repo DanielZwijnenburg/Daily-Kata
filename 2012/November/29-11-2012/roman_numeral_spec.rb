a418f301e8014ab067f2beb061b043efe98aadaa
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-29 08:34:18 +0100

    Should convert 1 to I refactor

--- 2012/November/29-11-2012/roman_numeral_spec.rb
+++ 2012/November/29-11-2012/roman_numeral_spec.rb
@@ -1,8 +1,8 @@
 require_relative 'roman_numeral'
 
 describe 'RomanNumeral' do
+  let(:numeral) {RomanNumeral.new}
   it "converts 1 to I" do
-    @numeral = RomanNumeral.new
-    @numeral.toRoman( 1 ).should eq "I"
+    numeral.toRoman(1).should eq "I"
   end
 end
