328bb6b9b66dd3f104487e36962aa35aa829dc33
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-30 18:09:38 +0100

    Should return buzz if dividable by five refactor

--- 2012/November/30-11-2012/spec/FizzBuzzSpec.js
+++ 2012/November/30-11-2012/spec/FizzBuzzSpec.js
@@ -1,12 +1,14 @@
 describe("FizzBuzz", function() {
 
+  beforeEach(function(){
+    fizzBuzz = new FizzBuzz();
+  })
+
   it("should return fizz when dividable by 3", function(){
-    var fizzBuzz = new FizzBuzz();
     expect(fizzBuzz.calculate(3)).toEqual("fizz");
   })
 
   it("should return buzz when dividable by 5", function(){
-    var fizzBuzz = new FizzBuzz();
     expect(fizzBuzz.calculate(5)).toEqual("buzz");
   })
 })
