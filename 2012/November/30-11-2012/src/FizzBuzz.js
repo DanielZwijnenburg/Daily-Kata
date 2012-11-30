a2505f23026c5c205072bfb6c4b26474b7717cbf
Daniel Zwijnenburg <daniel@danielz.nl>
2012-11-30 18:11:05 +0100

    Should return fizzbuzz if dividable by three and five green

--- 2012/November/30-11-2012/src/FizzBuzz.js
+++ 2012/November/30-11-2012/src/FizzBuzz.js
@@ -1,6 +1,8 @@
 FizzBuzz = function(){
   calculate = function(numberString) {
-    if(numberString % 3 == 0) {
+    if(numberString % 3 == 0 && numberString % 5 == 0){
+      return "fizzbuzz"
+    }else if(numberString % 3 == 0) {
       return "fizz";
     } else if(numberString % 5 == 0) {
       return "buzz";
