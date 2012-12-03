1ed194d862b71a185ee831de6ce9f9ade9f111d4
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-03 10:15:59 +0100

    It should return 6 with new line delimiter mixed in green

--- 2012/December/03-12-2012/string_calculator.rb
+++ 2012/December/03-12-2012/string_calculator.rb
@@ -1,6 +1,7 @@
 class StringCalculator
   def add(numberString)
     return 0 if numberString.empty?
+    numberString.gsub!("\n", ",")
     numbers = numberString.split(",").map(&:to_i)
 
     numbers.inject(&:+)
