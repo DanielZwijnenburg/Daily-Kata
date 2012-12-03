a1fae3ce42666e464895bd9ca2b8cd27824b337c
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-03 13:02:05 +0100

    It should return argument error if negative numbers are given green

--- 2012/December/03-12-2012/string_calculator.rb
+++ 2012/December/03-12-2012/string_calculator.rb
@@ -1,11 +1,16 @@
 class StringCalculator
   def add(numberString)
     return 0 if numberString.empty?
+
     custom_delimiter = numberString.include?("//") ? numberString.slice(2, numberString.index("\n") -2) : ","
 
     numberString.gsub!("\n", custom_delimiter)
 
     numbers = numberString.split(custom_delimiter).map(&:to_i)
+
+    negatives = numbers.find_all{|n| n < 0}
+    raise ArgumentError.new('Negatives not allowed #{negatives.join(" ")}') if  negatives.any?
+
 
     numbers.inject(&:+)
   end
