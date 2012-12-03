eba697c9331ad9351b6af23c0ef74c6757ab1b53
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-03 13:10:35 +0100

    It should return argument error if negative numbers are given refactor

--- 2012/December/03-12-2012/string_calculator.rb
+++ 2012/December/03-12-2012/string_calculator.rb
@@ -1,17 +1,32 @@
 class StringCalculator
+  attr_accessor :custom_delimiter, :number_array
+
   def add(numberString)
     return 0 if numberString.empty?
 
-    custom_delimiter = numberString.include?("//") ? numberString.slice(2, numberString.index("\n") -2) : ","
+    find_delimiter_and_replace(numberString)
+
+    split_number_string_and_store_result(numberString)
+
+    raise_error_if_negatives
+
+    @number_array.inject(&:+)
+  end
+
+  private
+
+  def find_delimiter_and_replace(numberString)
+    @custom_delimiter = numberString.include?("//") ? numberString.slice(2, numberString.index("\n") -2) : ","
 
     numberString.gsub!("\n", custom_delimiter)
+  end
 
-    numbers = numberString.split(custom_delimiter).map(&:to_i)
+  def split_number_string_and_store_result(numberString)
+    @number_array = numberString.split(@custom_delimiter).map(&:to_i)
+  end
 
-    negatives = numbers.find_all{|n| n < 0}
+  def raise_error_if_negatives
+    negatives = @number_array.find_all{|n| n < 0}
     raise ArgumentError.new('Negatives not allowed #{negatives.join(" ")}') if  negatives.any?
-
-
-    numbers.inject(&:+)
   end
 end
