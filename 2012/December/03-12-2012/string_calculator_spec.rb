99ba27dbdc75b34cc09e2becfcaeab8ea8975e81
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-03 10:01:59 +0100

    Daily Kata for December 2012, String Calculator

---
+++ 2012/December/03-12-2012/string_calculator_spec.rb
@@ -0,0 +1,25 @@
+require_relative 'string_calculator'
+
+describe 'StringCalculator' do
+
+  describe "#add" do
+    pending "implement"
+  end
+
+  describe "#add with \n delimiter mixed in" do
+    pending "implement"
+  end
+
+  describe "#add with custom delimiter" do
+    pending "implement"
+  end
+
+  describe "#add with negative number will raise exception" do
+    pending "implement"
+  end
+
+  describe "#add with numbers bigger than 1000 should be ignored" do
+    pending "implement"
+  end
+
+end
