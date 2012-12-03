fb672f9bb0c04d0d1a0963551c2435fd0fcd217a
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-03 12:56:51 +0100

    It should return correct number with custom delimiter green

--- 2012/December/03-12-2012/string_calculator.rb
+++ 2012/December/03-12-2012/string_calculator.rb
@@ -1,7 +1,11 @@
 class StringCalculator
   def add(numberString)
     return 0 if numberString.empty?
+    custom_delimiter = numberString.include?("//") ? numberString.slice(2, numberString.index("\n") -2) : ","
+
     numberString.gsub!("\n", ",")
+    numberString.gsub!(custom_delimiter, ",")
+
     numbers = numberString.split(",").map(&:to_i)
 
     numbers.inject(&:+)
