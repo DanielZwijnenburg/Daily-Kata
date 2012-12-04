eac857bae65868c0c9f055724ee4aa8a7745e4f3
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-04 10:51:11 +0100

    It should return the LCD representation of 2 refactor

--- 2012/December/04-12-2012/lcd_kata_spec.rb
+++ 2012/December/04-12-2012/lcd_kata_spec.rb
@@ -2,21 +2,27 @@ require_relative "lcd_kata"
 
 describe "LcdKata" do
   describe "#convert" do 
-
-    it "should return the LCD representation of 1" do
-      Lcd.new.convert(1).should ==  "   " +
-                                    "  |" +
-                                    "   " +
-                                    "  |" +
-                                    "   "
+    subject {Lcd.new.convert(number)}
+    context "should return the LCD representation of 1" do
+      let(:number) { 1 }
+      it { 
+            should == "   " +
+                      "  |" +
+                      "   " +
+                      "  |" +
+                      "   "
+          }
     end
 
-    it "#convert should return the LCD representation of 2" do
-      Lcd.new.convert(2).should ==  " - " +
-                                    "  |" +
-                                    " - " +
-                                    "|  " +
-                                    " - "
+    context "should return the LCD representation of 2" do
+      let(:number) { 2 }
+      it { 
+            should == " - " +
+                      "  |" +
+                      " - " +
+                      "|  " +
+                      " - "
+          }
     end
   end
 end
