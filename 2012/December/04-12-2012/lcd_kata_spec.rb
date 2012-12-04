146754f94f08fe78be7c8d327157d19d89864a3f
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-04 10:35:03 +0100

    It should return the LCD representation of 1 red

--- 2012/December/04-12-2012/lcd_kata_spec.rb
+++ 2012/December/04-12-2012/lcd_kata_spec.rb
@@ -1,4 +1,14 @@
 require_relative "lcd_kata"
 
 describe "LcdKata" do
+  describe "#convert" do 
+
+    it "should return the LCD representation of 1" do
+      Lcd.new.convert(1).should ==  "   " +
+                                    "  |" +
+                                    "   " +
+                                    "  |" +
+                                    "   "
+    end
+  end
 end
