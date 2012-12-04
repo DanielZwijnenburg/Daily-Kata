94384b06e389e5d6d604578e7a06b6c8eb41e750
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-04 11:24:22 +0100

    It should return the LCD representation of 1337 refactor

--- 2012/December/04-12-2012/lcd_kata.rb
+++ 2012/December/04-12-2012/lcd_kata.rb
@@ -1,4 +1,9 @@
 class Lcd
+  attr_accessor :numbers
+
+  def initialize
+    @numbers = []
+  end
   @@numbers = [
                 [" - ",
                  "| |",
@@ -52,12 +57,22 @@ class Lcd
                  " - "]
               ]
 
-  def convert(number)
-    numbers = number.to_s.each_char.map(&:to_i)
+  def convert(numbers)
+    build_number_array(numbers)
 
-    result = 5.times.inject([]) do |array, rowNumber|
-      line = numbers.inject([]) { |array, digit| array << @@numbers[digit][rowNumber] }
+    result = 5.times.inject([]) do |array, rowLine|
+      line = build_digit_line(rowLine)
       array << line.join(" ")
     end.join("")
+  end
+
+  private
+
+  def build_number_array(numbers)
+    @numbers = numbers.to_s.each_char.map(&:to_i)
+  end
+
+  def build_digit_line(rowLine)
+    @numbers.inject([]) { |array, digit| array << @@numbers[digit][rowLine] }
   end
 end
