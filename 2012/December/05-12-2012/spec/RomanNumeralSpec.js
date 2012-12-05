5cb53ccc2ac7c8476d24296c05d3e3e11f8e0c19
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-05 13:00:42 +0100

    It should convert 5 to V red

--- 2012/December/05-12-2012/spec/RomanNumeralSpec.js
+++ 2012/December/05-12-2012/spec/RomanNumeralSpec.js
@@ -3,4 +3,9 @@ describe("RomanNumeral", function() {
     romanNumeral = new RomanNumeral();
     expect(romanNumeral.convert(1)).toEqual("I");
   })
+
+  it('should return the roman numeral for 5', function() {
+    romanNumeral = new RomanNumeral();
+    expect(romanNumeral.convert(5)).toEqual("V");
+  })
 })
