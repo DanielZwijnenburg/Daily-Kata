e8212e2e71e327f1d3c772adb58ea5d2433f5d8d
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-05 13:00:29 +0100

    It should convert 1 to I refactor

--- 2012/December/05-12-2012/spec/RomanNumeralSpec.js
+++ 2012/December/05-12-2012/spec/RomanNumeralSpec.js
@@ -1,5 +1,5 @@
 describe("RomanNumeral", function() {
-  it('description', function() {
+  it('should return the roman numeral for 1', function() {
     romanNumeral = new RomanNumeral();
     expect(romanNumeral.convert(1)).toEqual("I");
   })
