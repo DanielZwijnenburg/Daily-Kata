6a73b3346f809711768ee6c703ca28907207e1ac
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-06 11:00:53 +0100

    Should return 0 for guttergame red

--- 2012/December/06-12-2012/bowling_game_spec.rb
+++ 2012/December/06-12-2012/bowling_game_spec.rb
@@ -2,4 +2,7 @@ require_relative("bowling_game.rb")
 
 describe "BowlingKata" do
 
+  it "should return 0 for guttergame" do
+    BowlingGame.new.score("--------------------").should eq 0
+  end
 end
