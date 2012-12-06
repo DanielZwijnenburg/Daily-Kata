934956741c394eae9d4838c5d3147ec389792fbf
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-06 11:16:33 +0100

    Should return 16 for 5 spare 3 green

--- 2012/December/06-12-2012/bowling_game.rb
+++ 2012/December/06-12-2012/bowling_game.rb
@@ -1,5 +1,25 @@
 class BowlingGame
+  attr_accessor :score_array
+
+  def initialize
+    @score_array = []
+  end
+
   def score(score_string)
-    score_string.chars.map(&:to_i).inject(&:+)
+    map_score_string_to_array(score_string.chars.to_a)
+    score_array.inject(&:+)
+  end
+
+  private
+
+  def map_score_string_to_array(score_string)
+    score_string.each_with_index do |score, i|
+      if score == "/"
+        @score_array << (10 - (score_string[i -1].to_i))
+        @score_array << score_string[i +1].to_i
+      else
+        @score_array << score.to_i
+      end
+    end
   end
 end
