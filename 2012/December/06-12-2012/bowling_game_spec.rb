419d8b0bda99e9c541ad2992787d1fb1a4603291
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-06 11:02:37 +0100

    Should return 10 for thrown score green

--- 2012/December/06-12-2012/bowling_game_spec.rb
+++ 2012/December/06-12-2012/bowling_game_spec.rb
@@ -5,4 +5,8 @@ describe "BowlingKata" do
   it "should return 0 for guttergame" do
     BowlingGame.new.score("--------------------").should eq 0
   end
+
+  it "should return 10 for thrown score" do
+    BowlingGame.new.score("5-5-----------------").should eq 10
+  end
 end
