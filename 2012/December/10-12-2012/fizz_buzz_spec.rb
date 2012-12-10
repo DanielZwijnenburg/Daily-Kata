d6cade9c07015f2864be6a29bd3e4bedcfafd90b
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-10 10:33:12 +0100

    Third test red

--- 2012/December/10-12-2012/fizz_buzz_spec.rb
+++ 2012/December/10-12-2012/fizz_buzz_spec.rb
@@ -6,13 +6,13 @@ describe 'FizzBuzz' do
     fizzbuzz.calculate(1).should == "1"
   end
 
-  # it "should return 'fizz' when number is dividable by 3" do
-  #   fizzbuzz.calculate(3).should == "fizz"
-  # end
+  it "should return 'fizz' when number is dividable by 3" do
+    fizzbuzz.calculate(3).should == "fizz"
+  end
 
-  # it "should return 'buzz' when number is dividable by 5" do
-  #   fizzbuzz.calculate(5).should == "buzz"
-  # end
+  it "should return 'buzz' when number is dividable by 5" do
+    fizzbuzz.calculate(5).should == "buzz"
+  end
 
   # it "should return 'fizzbuzz' when number is dividable by 15" do
   #   fizzbuzz.calculate(15).should == "fizzbuzz"
