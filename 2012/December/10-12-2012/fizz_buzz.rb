faae5dc58eb0054b6188d8bcd325cae09c5de9a5
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-10 10:34:33 +0100

    Fourth test green

--- 2012/December/10-12-2012/fizz_buzz.rb
+++ 2012/December/10-12-2012/fizz_buzz.rb
@@ -1,6 +1,8 @@
 class FizzBuzz
   def calculate(number)
-    if number % 5 == 0
+    if number % 5 == 0 && number % 3 == 0
+      return "fizzbuzz"
+    elsif number % 5 == 0
       return "buzz"
     elsif number % 3 == 0
       return "fizz"
