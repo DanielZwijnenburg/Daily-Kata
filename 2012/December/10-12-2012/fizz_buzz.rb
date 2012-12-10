b0eeda309c5607573b2dd3ca0606d12c1b8ec28a
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-10 10:35:18 +0100

    Fourth test refactor

--- 2012/December/10-12-2012/fizz_buzz.rb
+++ 2012/December/10-12-2012/fizz_buzz.rb
@@ -1,13 +1,23 @@
 class FizzBuzz
   def calculate(number)
-    if number % 5 == 0 && number % 3 == 0
+    if is_dividable_by_three?(number) and is_dividable_by_five?(number)
       return "fizzbuzz"
-    elsif number % 5 == 0
-      return "buzz"
-    elsif number % 3 == 0
+    elsif is_dividable_by_three?(number)
       return "fizz"
+    elsif is_dividable_by_five?(number)
+      return "buzz"
     else
       return number.to_s
     end
+  end
+
+  private
+
+  def is_dividable_by_three?(number)
+    number % 3 == 0
+  end
+
+  def is_dividable_by_five?(number)
+    number % 5 == 0
   end
 end
